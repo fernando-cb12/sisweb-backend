3ea1996cd55613813c671a2bd4597cd0c5486f52
fernando-cb12 <fernandocamoub@gmail.com>
2025-04-02 12:58:33 -0700

    feat: basic frontend and implemented postgres db connection

--- index.ts
+++ index.ts
@@ -2,10 +2,12 @@ import express, { Express } from "express";
 import apiRouter from "./src/routes";
 import connectionDB from "./src/connection/connection";
 import morgan from "morgan";
+import cors from "cors";
 
 const app: Express = express();
 const port = 3000;
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(apiRouter);
