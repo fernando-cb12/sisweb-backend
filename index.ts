db870d8246d83240256e8899fb719f71e51b81c4
fernando-cb12 <fernandocamoub@gmail.com>
2025-04-01 20:11:10 -0700

    feat: connection with SQL

--- index.ts
+++ index.ts
@@ -1,16 +1,19 @@
-import express, { Express, Request, Response } from "express";
-import apiRouter from "./src/routes/index";
+import express, { Express } from "express";
+import apiRouter from "./src/routes";
+import connectionDB from "./src/connection/connection";
+import morgan from "morgan";
 
 const app: Express = express();
-const morgan = require("morgan");
 const port = 3000;
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(apiRouter);
 
+connectionDB();
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on http://localhost:${port}`);
 });
 
 /*¿Qué valor guarda la constante app? ¿Por qué se declara como constante app?
